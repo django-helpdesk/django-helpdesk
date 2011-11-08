3d4c46704ae67eec414e1ff337d179b3f8355d61
Ross Poulton <ross@rossp.org>
2011-11-08 18:00:22 +1100

    Incrememtn version number & rebuild for PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 from setuptools import setup, find_packages
 
-version = '0.1.4b'
+version = '0.1.5b'
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
