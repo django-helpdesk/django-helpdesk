24754aa9d9f6021c0ba7efc0343047b0fdd4b800
Andreas Kotowicz <andreas.kotowicz@gmail.com>
2012-01-12 10:23:42 +0100

    fix HELPDESK_SHOW_EDIT_BUTTON_FOLLOW_UP in settings.py

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -83,7 +83,7 @@ HELPDESK_SUBMIT_A_TICKET_PUBLIC = getattr(settings, 'HELPDESK_SUBMIT_A_TICKET_PU
 HELPDESK_ALLOW_NON_STAFF_TICKET_UPDATE = getattr(settings, 'HELPDESK_ALLOW_NON_STAFF_TICKET_UPDATE', False)
 
 # show edit buttons in ticket follow ups.
-HELPDESK_SHOW_EDIT_BUTTON_FOLLOW_UP = getattr(settings, 'HELPDESK_HIDE_EDIT_BUTTON_FOLLOW_UP', True)
+HELPDESK_SHOW_EDIT_BUTTON_FOLLOW_UP = getattr(settings, 'HELPDESK_SHOW_EDIT_BUTTON_FOLLOW_UP', True)
 
 # show ticket edit button on top of ticket description.
 HELPDESK_SHOW_EDIT_BUTTON_TICKET_TOP = getattr(settings, 'HELPDESK_SHOW_EDIT_BUTTON_TICKET_TOP', True)
