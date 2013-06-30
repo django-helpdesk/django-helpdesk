a086b5592c1a1cd94e4744027ac6180095122314
Ross Poulton <ross@rossp.org>
2013-07-01 07:51:42 +1000

    Bump verison number for PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 from setuptools import setup, find_packages
 
-version = '0.1.8'
+version = '0.1.9'
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
