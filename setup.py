0473ff089a7f4b402846293f95e37f7d1459f058
Ross Poulton <ross@rossp.org>
2013-09-16 11:48:20 +1000

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 from setuptools import setup, find_packages
 
-version = '0.1.9'
+version = '0.1.10'
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
