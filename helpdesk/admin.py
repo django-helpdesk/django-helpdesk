7894eea1e184a58adb469a5728bc59daede7b9f4
Alex Barcelo <alex.barcelo@bsc.es>
2015-12-18 14:56:53 +0100

    Making Queue slug unique and improving certain admin-related behaviour

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -7,6 +7,7 @@ from helpdesk.models import CustomField
 
 class QueueAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'email_address', 'locale')
+    prepopulated_fields = {"slug": ("title",)}
 
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'assigned_to', 'queue', 'hidden_submitter_email',)
