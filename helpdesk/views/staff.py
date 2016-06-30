3b181e87efd9a1a4df09c587dc440a60713ea38c
Daryl <daryl.x18@kawhai.net>
2016-06-30 13:39:51 +1200

    Fix inconsistent indentation

--- helpdesk/views/staff.py
+++ helpdesk/views/staff.py
@@ -862,7 +862,7 @@ def ticket_list(request):
     try:
         page = int(request.GET.get('page', '1'))
     except ValueError:
-         page = 1
+        page = 1
 
     try:
         tickets = ticket_paginator.page(page)
