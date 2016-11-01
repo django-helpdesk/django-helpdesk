399b0901f6ad4c30170ee96044f60f7197cf12b9
Alex Seeholzer <seeholzer@gmail.com>
2016-11-01 22:34:45 +0100

    newer version of tests

--- helpdesk/tests/test_get_email.py
+++ helpdesk/tests/test_get_email.py
@@ -18,6 +18,10 @@ except ImportError:
     # Python < 3.3
     import mock
 
+unrouted_socks_server = "127.0.0.1"
+unrouted_email_server = "127.0.0.1"
+unrouted_port = "12345"
+
 
 class GetEmailCommonTests(TestCase):
 
@@ -41,15 +45,19 @@ class GetEmailParametricTemplate(object):
             "slug": 'QQ',
             "allow_public_submission": True,
             "allow_email_submission": True,
-            "email_box_type": self.method}
+            "email_box_type": self.method,
+            "logging_type": 'none'}
 
         if self.method == 'local':
             kwargs["email_box_local_dir"] = '/var/lib/mail/helpdesk/'
+        else:
+            kwargs["email_box_host"] = unrouted_email_server
+            kwargs["email_box_port"] = unrouted_port
 
         if self.socks:
             kwargs["socks_proxy_type"] = self.socks
-            kwargs["socks_proxy_host"] = "127.0.0.1"
-            kwargs["socks_proxy_host"] = "9150"  # default for tor
+            kwargs["socks_proxy_host"] = unrouted_socks_server
+            kwargs["socks_proxy_port"] = unrouted_port
 
         self.queue_public = Queue.objects.create(**kwargs)
 
@@ -57,35 +65,55 @@ class GetEmailParametricTemplate(object):
         """Tests reading emails from a queue and creating tickets."""
         test_email = "To: update.public@example.com\nFrom: comment@example.com\nSubject: Some Comment\n\nThis is the helpdesk comment via email."
 
-        # Test local email reading
-        if self.method == 'local':
-            with mock.patch('helpdesk.management.commands.get_email.listdir') as mocked_listdir, \
-                    mock.patch('helpdesk.management.commands.get_email.isfile') as mocked_isfile, \
-                    mock.patch('builtins.open' if six.PY3 else '__builtin__.open', mock.mock_open(read_data=test_email)):
-                mocked_isfile.return_value = True
-                mocked_listdir.return_value = ['filename1', 'filename2']
-
+        if self.socks:
+            from socks import ProxyConnectionError
+            with self.assertRaisesRegexp(ProxyConnectionError, '%s:%s' % (unrouted_socks_server, unrouted_port)):
                 call_command('get_email')
 
-                mocked_listdir.assert_called_with('/var/lib/mail/helpdesk/')
-                mocked_isfile.assert_any_call('/var/lib/mail/helpdesk/filename1')
-                mocked_isfile.assert_any_call('/var/lib/mail/helpdesk/filename2')
-
-        # Other methods go here, not implemented yet.
+            # with mock.patch('socket.socket') as mocked_socket,\
+            #         mock.patch('socks.socket') as mocked_sockssocket:
+            #     call_command('get_email')
+            #     print(mocked_socket)
+            #     print(mocked_sockssocket)
         else:
-            return True
+            # Test local email reading
+            if self.method == 'local':
+                with mock.patch('helpdesk.management.commands.get_email.listdir') as mocked_listdir, \
+                        mock.patch('helpdesk.management.commands.get_email.isfile') as mocked_isfile, \
+                        mock.patch('builtins.open' if six.PY3 else '__builtin__.open', mock.mock_open(read_data=test_email)):
+                    mocked_isfile.return_value = True
+                    mocked_listdir.return_value = ['filename1', 'filename2']
 
-        ticket1 = get_object_or_404(Ticket, pk=1)
-        self.assertEqual(ticket1.ticket_for_url, "QQ-%s" % ticket1.id)
-        self.assertEqual(ticket1.description, "This is the helpdesk comment via email.")
+                    call_command('get_email')
 
-        ticket2 = get_object_or_404(Ticket, pk=2)
-        self.assertEqual(ticket2.ticket_for_url, "QQ-%s" % ticket2.id)
-        self.assertEqual(ticket2.description, "This is the helpdesk comment via email.")
+                    mocked_listdir.assert_called_with('/var/lib/mail/helpdesk/')
+                    mocked_isfile.assert_any_call('/var/lib/mail/helpdesk/filename1')
+                    mocked_isfile.assert_any_call('/var/lib/mail/helpdesk/filename2')
+
+            elif self.method == 'pop3':
+                if six.PY3:
+                    errorclass = ConnectionRefusedError
+                else:
+                    from socket import error
+                    errorclass = error
+                with self.assertRaisesRegexp(errorclass, "Connection refused"):
+                    call_command('get_email')
+
+            # Other methods go here, not implemented yet.
+            else:
+                return True
+
+            ticket1 = get_object_or_404(Ticket, pk=1)
+            self.assertEqual(ticket1.ticket_for_url, "QQ-%s" % ticket1.id)
+            self.assertEqual(ticket1.description, "This is the helpdesk comment via email.")
+
+            ticket2 = get_object_or_404(Ticket, pk=2)
+            self.assertEqual(ticket2.ticket_for_url, "QQ-%s" % ticket2.id)
+            self.assertEqual(ticket2.description, "This is the helpdesk comment via email.")
 
 # build matrix of test cases
 case_methods = [c[0] for c in Queue._meta.get_field('email_box_type').choices]
-case_socks = ("SOCKS4", "SOCKS5", False)
+case_socks = [False] + [c[0] for c in Queue._meta.get_field('socks_proxy_type').choices]
 case_matrix = list(itertools.product(case_methods, case_socks))
 
 # Populate TestCases from the matrix of parameters
