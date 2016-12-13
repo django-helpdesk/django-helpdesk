f5b68772cfd5262a506a9d8456133dc2101e785b
Pawel M <m@rkowski.pl>
2016-12-13 23:31:26 +0100

    Downloaded attachment was not decoded and additionally encoded by default django method (its size after download is bigger than originally attached file). This is the reason why it is illegible

--- helpdesk/management/commands/get_email.py
+++ helpdesk/management/commands/get_email.py
@@ -319,8 +319,7 @@ def ticket_from_message(message, queue, logger):
             if not name:
                 ext = mimetypes.guess_extension(part.get_content_type())
                 name = "part-%i%s" % (counter, ext)
-            print(name, part.get_content_type())
-            files.append(SimpleUploadedFile(name, part.get_payload(decode=True), part.get_content_type()))
+            files.append(SimpleUploadedFile(name, part.get_payload(decode=True), mimetypes.guess_type(name)[0]))
             logger.debug("Found MIME attachment %s" % name)
 
         counter += 1
