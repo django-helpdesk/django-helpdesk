92ff979dfa32d69de6a6fc1f809790c3961c692c
Garret Wassermann <gwasser@gmail.com>
2017-10-24 04:04:00 -0400

    Set dev version to 0.3.0-dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 from setuptools import setup, find_packages
 
-version = '0.2.2'
+version = '0.3.0.dev1'
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
