c085dba901cafe2f22fa3ab7b465f57beaf32b7d
Garret Wassermann <gwasser@gmail.com>
2017-12-06 02:21:46 -0500

    Trying to actually fix naked except

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 try:
     DEFAULT_USER_SETTINGS = settings.HELPDESK_DEFAULT_SETTINGS
-except ImproperlyConfigured, AttributeError:
+except AttributeError:
     DEFAULT_USER_SETTINGS = None
 
 if not isinstance(DEFAULT_USER_SETTINGS, dict):
