c314d515c16da8ba97c257868b12ad75a5eac465
Garret Wassermann <gwasser@gmail.com>
2019-03-09 18:42:10 -0500

    Fix PEP8 in url.py changes

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -194,13 +194,13 @@ urlpatterns += [
             template_name='helpdesk/registration/login.html',
             next_page='../'),
         name='logout'),
-        
+
     url(r'^password_change/$',
         auth_views.PasswordChangeView.as_view(
             template_name='helpdesk/registration/change_password.html',
             success_url='./done'),
         name='password_change'),
-    
+
     url(r'^password_change/done$',
         auth_views.PasswordChangeDoneView.as_view(
             template_name='helpdesk/registration/change_password_done.html',),
