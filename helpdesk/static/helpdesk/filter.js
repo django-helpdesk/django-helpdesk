e7465ab2fdad6620daf5273183c04e9818ae46b7
Jachym Cepicky <jachym.cepicky@gmail.com>
2019-07-06 22:24:08 +0200

    more redminenish ticket filters

--- helpdesk/static/helpdesk/filter.js
+++ helpdesk/static/helpdesk/filter.js
@@ -16,6 +16,27 @@ $(document).ready(function() {
         $(boxName).children("select").each(function() {
             this.selectedIndex = -1;
         });
+
+        var selectId = $(this).parents(".filterBox").attr('id');
+        var attr = selectId.replace("filterBox", "");
+        $("#filterBuilderSelect-" + attr)[0].disabled = "";
+
         return false;
     });
 });
+
+/**
+ * Called, when filterBuilderSelect input changed - will make input div appear
+ * to the user. Also disable this option in the Add filter menu
+ *
+ * @param {string} val name of selected filter value
+ */
+var onFilterChange = function(val) {
+  if (val) {
+    var boxName = "#filterBox" + val;
+    $(boxName).slideDown();
+    $(boxName)[0].style.display="block";
+
+    $("#filterBuilderSelect-" + val)[0].disabled = "disabled";
+  }
+};
