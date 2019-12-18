860a30486c2fda7656c5cf648871730f37f05ede
Garret Wassermann <gwasser@gmail.com>
2019-12-17 22:38:03 -0500

    Comments pass PEP-8

--- helpdesk/tests/test_attachments.py
+++ helpdesk/tests/test_attachments.py
@@ -110,18 +110,18 @@ class AttachmentUnitTests(TestCase):
         self.assertEqual(filename, self.file_attrs['filename'])
 
     # TODO: FIXME: what's wrong with this test that we get integrity errors?
-    #@mock.patch('helpdesk.lib.FollowUpAttachment', autospec=True)
-    #def test_autofill(self, mock_att_save, mock_queue_save, mock_ticket_save, mock_follow_up_save):
-    #    """ check utf-8 data is parsed correctly """
-    #    self.follow_up.pk = 100
-    #    self.follow_up.save()
-    #    obj = models.FollowUpAttachment.objects.create(
-    #        followup=self.follow_up,
-    #        file=self.test_file
-    #    )
-    #    self.assertEqual(obj.filename, self.file_attrs['filename'])
-    #    self.assertEqual(obj.size, len(self.file_attrs['content']))
-    #    self.assertEqual(obj.mime_type, "text/plain")
+    # @mock.patch('helpdesk.lib.FollowUpAttachment', autospec=True)
+    # def test_autofill(self, mock_att_save, mock_queue_save, mock_ticket_save, mock_follow_up_save):
+    #     """ check utf-8 data is parsed correctly """
+    #     self.follow_up.pk = 100
+    #     self.follow_up.save()
+    #     obj = models.FollowUpAttachment.objects.create(
+    #         followup=self.follow_up,
+    #         file=self.test_file
+    #     )
+    #     self.assertEqual(obj.filename, self.file_attrs['filename'])
+    #     self.assertEqual(obj.size, len(self.file_attrs['content']))
+    #     self.assertEqual(obj.mime_type, "text/plain")
 
     def test_kbi_attachment(self, mock_att_save, mock_queue_save, mock_ticket_save):
         """ check utf-8 data is parsed correctly """
