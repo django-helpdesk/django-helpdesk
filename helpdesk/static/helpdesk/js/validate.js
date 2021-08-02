ad30bf98558271eb29533160dfa8b76796708d83
Elizabeth Beers <ebeers86@gmail.com>
2021-08-02 14:46:42 -0400

    Forms: Updated/fixed validation.js

--- helpdesk/static/helpdesk/js/validate.js
+++ helpdesk/static/helpdesk/js/validate.js
@@ -1,31 +1,28 @@
 //TODO: 'High Performing' is not a type of property -- will need to add an array of 'high performing' types.
 
-$('#id_e_is_affordable_housing').hide();
-$('#id_e_property_type').change(function(){
-    if($('#id_e_property_type').val() == "Multifamily Housing") {
-        $('#id_e_is_affordable_housing').show();
-    } else {
-        $('#id_e_is_affordable_housing').hide();
-    }
-});
+/* Variables */
 
-$('#id_e_type_affordable_housing').hide();
-$('#id_e_property_type').change(function(){
-    if($('#id_e_property_type').val() == "Multifamily Housing" && $('#id_e_is_affordable_housing').val()) {
-        $('#id_e_type_affordable_housing').show();
-    } else {
-        $('#id_e_type_affordable_housing').hide();
-    }
-});
+var key = {
+    affordable_boolean: 'e_is_affordable_housing',
+    affordable_list: 'e_type_affordable_housing',
+    acp_list: 'e_acp_type',
+    property_list: 'e_property_type',
+    pathway_list: 'e_pathway',
+};
+var id = {
+    affordable_boolean: '#id_' + key.affordable_boolean,
+    affordable_list: '#id_' + key.affordable_list,
+    acp_list: '#id_' + key.acp_list,
+    property_list: '#id_' + key.property_list,
+    pathway_list: '#id_' + key.pathway_list,
+};
+var group = {
+    affordable_boolean: '#id_group_' + key.affordable_boolean,
+    affordable_list: '#id_group_' + key.affordable_list,
+    acp_list: '#id_group_' + key.acp_list,
+};
 
-$('#id_e_acp_type').hide();
-$('#id_e_property_type').change(function(){
-    if($('#id_e_pathway').val() == "Alternative Compliance Pathway") {
-        $('#id_e_acp_type').show();
-    } else {
-        $('#id_e_acp_type').hide();
-    }
-});
+/* Alerts */
 
 var alertMsg1 = 'This property type is not eligible for the Standard Target Pathway, please select another pathway.';
 var alertMsg2 = 'You have selected the Prescriptive Pathway, please note that DOEE will not approve this selection until the building owner has submitted an energy audit. For more information please see this FAQ [link to be provided later].';
@@ -44,25 +41,21 @@ jQuery.validator.addMethod('alert2', function (value, element) {
     return ok;
 }, alertMsg2);
 
-/*
-This field appears
-if "ACP Option" =  "Extended Deep Energy Retrofit"
-AND ("Affordable Housing/Rent Controlled?" = FALSE
-OR ("Primary Property Type - Portfolio Manager Calculated" != "College/University" OR "Hospital").
-If this alert text appears the user should not be able to submit the form, until they select another pathway
-*/
+/*  This field appears
+    if "ACP Option" =  "Extended Deep Energy Retrofit"
+    AND ("Affordable Housing/Rent Controlled?" = FALSE
+    OR ("Primary Property Type - Portfolio Manager Calculated" != "College/University" OR "Hospital").
+    If this alert text appears the user should not be able to submit the form, until they select another pathway */
 jQuery.validator.addMethod('alert3', function (value, element, param) {
-    return this.optional(element) || !( value == "Extended Deep Energy Retrofit" && ( !$(param.affordable).val() || ($(param.prop).val() != "College/University" || $(param.prop).val() != "Hospital")) );
+    return this.optional(element) || !( value == "Extended Deep Energy Retrofit" && ( !$(param.affordable).prop('checked') || ($(param.prop).val() != "College/University" || $(param.prop).val() != "Hospital")) );
 }, alertMsg3);
 
-/*
-if "ACP Option" =  "Extended Deep Energy Retrofit"
-AND ("Affordable Housing/Rent Controlled?" = TRUE
-OR ("Primary Property Type - Portfolio Manager Calculated" = "College/University" OR "Hospital").
-If this alert text appears the user should not be able to submit the form, until they select another pathway
-*/
+/*   if "ACP Option" =  "Extended Deep Energy Retrofit"
+    AND ("Affordable Housing/Rent Controlled?" = TRUE
+    OR ("Primary Property Type - Portfolio Manager Calculated" = "College/University" OR "Hospital").
+    If this alert text appears the user should not be able to submit the form, until they select another pathway */
 jQuery.validator.addMethod('alert4', function (value, element, param) {
-    return this.optional(element) || !( value == "Extended Deep Energy Retrofit" && ( $(param.affordable).val() || ($(param.prop).val() == "College/University" || $(param.prop).val() == "Hospital")) );
+    return this.optional(element) || !( value == "Extended Deep Energy Retrofit" && ( $(param.affordable).prop('checked') || ($(param.prop).val() == "College/University" || $(param.prop).val() == "Hospital")) );
 }, alertMsg4);
 
 jQuery.validator.addMethod('alert5', function (value, element) {
@@ -72,50 +65,98 @@ jQuery.validator.addMethod('alert5', function (value, element) {
     return ok;
 }, alertMsg5);
 
+
+/* Validations */
+
 var form_validator = $('#ticket_submission_form').validate({
     ignore: '.error-okay',
     rules: {
-        e_pathway: {
+        [key.pathway_list]: {
             alert2: true,
             alert5: true
         },
-        e_property_type: {
+        [key.property_list]: {
             alert1: {
                 param: {
-                    path: '#id_e_pathway',
-                    prop: '#id_e_property_type'
+                    path: id.pathway_list,
+                    prop: id.property_list
                 }
             }
         },
-        e_acp: {
+        [key.acp_list]: {
             alert3: {
                 param: {
-                    affordable: '#id_e_is_affordable_housing',
-                    prop: '#id_e_property_type'
+                    affordable: id.affordable_boolean,
+                    prop: id.property_list
                 }
             },
             alert4: {
                 param: {
-                    affordable: '#id_e_is_affordable_housing',
-                    prop: '#id_e_property_type'
+                    affordable: id.affordable_boolean,
+                    prop: id.property_list
                 }
             },
         }
     },
 });
 
-$('#id_e_pathway').change(function(){
-    if ($('#id_e_property_type').val() != '')
-        form_validator.element('#id_e_property_type');
+
+/* Hiding/showing elements based on other fields */
+
+$(group.affordable_boolean).hide();
+$(group.affordable_list).hide();
+$(group.acp_list).hide();
+
+$(id.property_list).change(function(){
+    //Alert 3 must be checked when either of two fields changes
+     if ($(id.acp_list).val() != '' && $(id.affordable_boolean).prop('checked'))
+        form_validator.element(id.acp_list);
+
+    //affordable_boolean field appears if the property type is Multifamily Housing
+    if($(id.property_list).val() == "Multifamily Housing") {
+        $(group.affordable_boolean).show();
+    } else {
+        $(group.affordable_boolean).hide();
+        $(id.affordable_boolean).prop('checked', false);
+    }
+
+    //affordable_list field appears if "Primary Property Type - Portfolio Manager Calculated" = "Multifamily Housing" AND "Affordable Housing" = TRUE
+    if($(id.property_list).val() == "Multifamily Housing" && $(id.affordable_boolean).prop('checked')) {
+        $(group.affordable_list).show();
+    } else {
+        $(group.affordable_list).hide();
+        $(id.affordable_list).val('');
+    }
 });
 
-// alert3 and alert4
-$('#id_e_property_type').change(function(){
-    if ($('#id_e_acp').val() != '' && $('#id_e_is_affordable_housing').val() != null)
-        form_validator.element('#id_e_acp');
-});
-$('#id_e_is_affordable_housing').change(function(){
-    if ($('#id_e_acp').val() != '' && $('#id_e_property_type').val() != '')
-        form_validator.element('#id_e_acp');
+$(id.affordable_boolean).change(function(){
+    //Alert 4 must be checked when either of two fields changes
+    if ($(id.acp_list).val() != '' && $(id.property_list).val() != '')
+        form_validator.element(id.acp_list);
+
+    //affordable_list field appears if "Primary Property Type - Portfolio Manager Calculated" = "Multifamily Housing" AND "Affordable Housing" = TRUE
+    if($(id.property_list).val() == "Multifamily Housing" && $(id.affordable_boolean).prop('checked')) {
+        $(group.affordable_list).show();
+    } else {
+        $(group.affordable_list).hide();
+        $(id.affordable_list).val('');
+    }
 });
 
+$(id.pathway_list).change(function(){
+    //removes class 'error-okay' so that validator will not ignore changes
+    $(id.pathway_list).removeClass("error-okay");
+    form_validator.element(id.pathway_list);
+
+    //Alert 1 must be checked when either of two fields changes
+    if ($(id.property_list).val() != '')
+        form_validator.element(id.property_list);
+
+    //acp_list field appears if "Pathway Selected" = "Alternative Compliance Pathway"
+    if($(id.pathway_list).val() == "Alternative Compliance Pathway") {
+        $(group.acp_list).show();
+    } else {
+        $(group.acp_list).hide();
+        $(id.acp_list).val('');
+    }
+});
