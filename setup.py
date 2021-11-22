94902ec44fc93d1e9fd2858cc2327902d028137a
Garret Wassermann <gwasser@gmail.com>
2021-11-21 23:37:25 -0500

    Bump version to 0.3.2 for bugfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.util import convert_path
 from fnmatch import fnmatchcase
 from setuptools import setup, find_packages
 
-version = '0.3.1'
+version = '0.3.2'
 
 # Provided as an attribute, so you can append to these instead
 # of replicating them:
