ae73fec2a0a5a024028d218db00e9cdb488c2a5e
Garret Wassermann <gwasser@users.noreply.github.com>
2021-12-31 22:21:28 -0500

    Spellchecking in validators.py

--- helpdesk/validators.py
+++ helpdesk/validators.py
@@ -4,6 +4,8 @@
 
 from django.conf import settings
 
+#TODO: can we use the builtin Django validator instead?
+# see: https://docs.djangoproject.com/en/4.0/ref/validators/#fileextensionvalidator
 def validate_file_extension(value):
     import os
     from django.core.exceptions import ValidationError
@@ -11,13 +13,13 @@ def validate_file_extension(value):
     # TODO: we might improve this with more thorough checks of file types
     # rather than just the extensions.
 
-    # check if VALID_EXTENSTIONS is defined in settings.py
+    # check if VALID_EXTENSIONS is defined in settings.py
     # if not use defaults
 
-    if settings.VALID_EXTENSTIONS:
-        valid_extenstions = settings.VALID_EXTENSTIONS
+    if settings.VALID_EXTENSIONS:
+        valid_extensions = settings.VALID_EXTENSIONS
     else:
-        valid_extenstions = ['.txt', '.pdf', '.doc', '.docx', '.odt', '.jpg', '.png']
+        valid_extensions = ['.txt', '.pdf', '.doc', '.docx', '.odt', '.jpg', '.png']
 
-    if not ext.lower() in valid_extenstions:
+    if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
