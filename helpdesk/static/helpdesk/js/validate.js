2d62ccc2d921324be54322a6f4d002158b8f0152
lcontreras <luiscontrerasorendain@gmail.com>
2022-05-31 17:22:12 -0400

    form validation: added ann arbor extension request changes

--- helpdesk/static/helpdesk/js/validate.js
+++ helpdesk/static/helpdesk/js/validate.js
@@ -3,33 +3,27 @@
 /* Variables */
 
 var key = {
+    // DC Specific
     affordable_boolean: 'e_is_affordable_housing',
     affordable_list: 'e_type_affordable_housing',
-//LOCAL    affordable_boolean: 'is_affordable_housing',
-//LOCAL    affordable_list: 'type_affordable_housing',
     acp_list: 'e_acp_type',
     property_list: 'e_property_type',
-//LOCAL	property_list: 'property_type',
     pathway_list: 'e_pathway',
     backup_pathway_list: 'e_backup_pathway',
     attachment: 'attachment',
+
+    // Ann Arbor Specific
+    ext_or_exempt: 'e_extension_or_exemption',
+    ext_reason: 'e_extension_reason'
 };
-var id = {
-    affordable_boolean: '#id_' + key.affordable_boolean,
-    affordable_list: '#id_' + key.affordable_list,
-    acp_list: '#id_' + key.acp_list,
-    property_list: '#id_' + key.property_list,
-    pathway_list: '#id_' + key.pathway_list,
-    backup_pathway_list: '#id_' + key.backup_pathway_list,
-    attachment: '#id_' + key.attachment,
-};
-var group = {
-    affordable_boolean: '#id_group_' + key.affordable_boolean,
-    affordable_list: '#id_group_' + key.affordable_list,
-    acp_list: '#id_group_' + key.acp_list,
-    backup_pathway_list: '#id_group_' + key.backup_pathway_list,
-    attachment: '#id_group_' + key.attachment,
-};
+
+var id = {};
+var group = {};
+Object.entries(key).forEach(function (e) {
+  id[e[0]] ='#id_' + e[1];
+  group[e[0]] = '#id_group_' + e[1];
+})
+
 var high_perf = ['Adult Education','Ambulatory Surgical Center','Multifamily Housing','Automobile Dealership','Bank Branch','Bar/Nightclub','Barracks','Courthouse','College/University','Convenience Store with Gas Station','Convenience Store without Gas Station','Financial Office','Data Center','Drinking Water Treatment & Distribution','Enclosed Mall','Office','Fast Food Restaurant','Food Sales','Food Service','Hospital (General Medical & Surgical)','Hotel','Laboratory','Lifestyle Center','Medical Office','Other - Education','Other - Lodging/Residential','Other - Mall','Other - Restaurant/Bar','Other - Specialty Hospital','Outpatient Rehabilitation/Physical Therapy','Pre-school/Daycare','Prison/Incarceration','Residence Hall/Dormitory','Residential Care Facility','Restaurant','Retail Store','Self-Storage Facility','Senior Care Community (also know as Senior Livi Community)','Strip Mall','Supermarket/Grocery Store','Urgent Care/Clinic/Other Outpatient','Veterinary Office','Vocational School','Wastewater Treatment Plant','Wholesale Club/Supercenter']
 
 /* Alerts */
@@ -115,8 +109,7 @@ var form_validator = $('#ticket_submission_form').validate({
 });
 
 
-/* Hiding/showing elements based on other fields */
-
+/* Hiding/showing elements in DC-Specific forms based on other fields */
 $(group.affordable_boolean).hide();
 $(group.affordable_list).hide();
 if ($(id.pathway_list).val() != "Alternative Compliance Pathway") {
@@ -213,3 +206,34 @@ $(id.backup_pathway_list).change(function() {
         form_validator.element(id.backup_pathway_list);
 });
 
+
+
+/* Hiding/showing elements in Ann Arbor-Specific forms based on other fields */
+var options = $(id.ext_reason).children();
+var values = $.map(options ,function(option) {
+    if (option.value != '' && option.value != '-----') {
+      return option.value;
+    }
+});
+ext_values = values.slice(0, 4)
+exempt_values = values.slice(4, )
+
+$(group.ext_reason).hide();
+$(id.ext_or_exempt).change(function () {
+  if ($(id.ext_or_exempt).val() != '') {
+    $(group.ext_reason).show();
+    // Modify Options based on Exempt or Extension
+    $(id.ext_reason).empty();   // Remove all choices
+    values_to_add = $(id.ext_or_exempt).val() == 'Extension' ? ext_values : exempt_values;
+    $(id.ext_reason).append($("<option></option>").attr("value", '').text('---------'));    // Empty first choice
+    $.each(values_to_add, function(index, val) {
+      $(id.ext_reason).append($("<option></option>").attr("value", val).text(val));   // add in specific choices
+    });
+  } else {
+    // Reset options
+    $(group.ext_reason).hide();
+    $(id.ext_reason).val('')
+  }
+
+
+});
