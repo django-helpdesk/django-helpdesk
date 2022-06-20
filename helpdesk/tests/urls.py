2910664950848c642cea9a22cba2dc5311af8171
Martin Whitehouse <martin@umed.org>
2022-06-20 16:34:32 +0200

    Fix path for tests

--- helpdesk/tests/urls.py
+++ helpdesk/tests/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('helpdesk/', include('helpdesk.urls', namespace='helpdesk')),
+    path('', include('helpdesk.urls', namespace='helpdesk')),
     path('admin/', admin.site.urls),
 ]
