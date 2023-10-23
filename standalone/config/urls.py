d1cce7caadf2479641d837eab809028641e9438a
Timothy Hobbs <timothy@hobbs.cz>
2023-10-23 14:00:03 +0200

    Add ability to configure local_urls

--- standalone/config/urls.py
+++ standalone/config/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from .local_urls import local_urlpatterns
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('helpdesk.urls', namespace='helpdesk')),
     path('api/auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + local_urlpatterns
