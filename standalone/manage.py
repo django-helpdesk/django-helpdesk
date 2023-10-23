7e0a335684d42eb76419ed4d03eecb393dc46074
Timothy Hobbs <timothy@hobbs.cz>
2023-10-23 13:42:18 +0200

    Switch to local_settings first for manage.py as well

--- standalone/manage.py
+++ standalone/manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "standalone.config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "standalone.config.local_settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
