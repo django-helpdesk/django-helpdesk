5f8864c0b15296d5dcf9a2285a1746ea93c9fe19
vineelreddy10 <vneelreddykamireddy@gmail.com>
2024-07-21 12:21:39 +0530

    inst_db_pending

--- standalone/entrypoint.sh
+++ standalone/entrypoint.sh
@@ -1,27 +1,63 @@
+# #!/bin/bash
+# # Install extra deps from /opt/extra-deps.txt if it exists
+# if [ -f /opt/extra-dependencies.txt ]; then
+#     pip install -r /opt/extra-dependencies.txt
+# fi
+
+# cd /opt/django-helpdesk/standalone/
+# if python manage.py showmigrations | grep '\[ \]\|^[a-z]' | grep '[  ]' -B 1; then
+#     python manage.py migrate --noinput                # Apply database migrations
+# fi
+
+# # Starting cron to check emails
+# printenv > /etc/env
+# env | awk -F= '{printf "export %s=\"%s\"\n", $1, $2}' > /etc/env
+# cron &&
+# # Start Gunicorn processes
+# echo Starting Gunicorn.
+# exec gunicorn standalone.config.wsgi:application \
+# 	   --name django-helpdesk \
+# 	   --bind 0.0.0.0:${GUNICORN_PORT:-"8000"} \
+# 	   --workers ${GUNICORN_NUM_WORKERS:-"6"} \
+# 	   --timeout ${GUNICORN_TIMEOUT:-"60"} \
+# 	   --preload \
+# 	   --log-level=debug \
+# 	   --log-file=- \
+# 	   --access-logfile=- \
+# 	   "$@"
+
+
 #!/bin/bash
-# Install extra deps from /opt/extra-deps.txt if it exists
+set -e
+
+# Install extra deps from /opt/extra-dependencies.txt if it exists
 if [ -f /opt/extra-dependencies.txt ]; then
     pip install -r /opt/extra-dependencies.txt
 fi
 
 cd /opt/django-helpdesk/standalone/
+
+# Apply database migrations if needed
 if python manage.py showmigrations | grep '\[ \]\|^[a-z]' | grep '[  ]' -B 1; then
-    python manage.py migrate --noinput                # Apply database migrations
+    python manage.py migrate --noinput
 fi
 
-# Starting cron to check emails
+# Export environment variables for cron jobs
 printenv > /etc/env
 env | awk -F= '{printf "export %s=\"%s\"\n", $1, $2}' > /etc/env
-cron &&
+
+# Start cron service
+service cron start
+
 # Start Gunicorn processes
-echo Starting Gunicorn.
+echo "Starting Gunicorn."
 exec gunicorn standalone.config.wsgi:application \
-	   --name django-helpdesk \
-	   --bind 0.0.0.0:${GUNICORN_PORT:-"8000"} \
-	   --workers ${GUNICORN_NUM_WORKERS:-"6"} \
-	   --timeout ${GUNICORN_TIMEOUT:-"60"} \
-	   --preload \
-	   --log-level=debug \
-	   --log-file=- \
-	   --access-logfile=- \
-	   "$@"
+    --name django-helpdesk \
+    --bind 0.0.0.0:${GUNICORN_PORT:-"8000"} \
+    --workers ${GUNICORN_NUM_WORKERS:-"6"} \
+    --timeout ${GUNICORN_TIMEOUT:-"60"} \
+    --preload \
+    --log-level=debug \
+    --log-file=- \
+    --access-logfile=- \
+    "$@"
