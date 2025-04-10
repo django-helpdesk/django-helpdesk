87a608e8d6b1f56e092f6c88fec537cf631448aa
PrM0d3rn <prostomodern@outlook.com>
2025-04-10 22:16:29 +0300

    added email config

--- standalone/config/local_settings.py
+++ standalone/config/local_settings.py
@@ -19,3 +19,10 @@ DATABASES = {
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
