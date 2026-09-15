f846563327f8d01a5fc21ceea2a60514bc1bcc7a
Gurupratap Matharu <38536783+gurupratap-matharu@users.noreply.github.com>
2026-09-15 08:31:30 +0530

    Add endpoints that map to django views

--- src/helpdesk/views/auth.py
+++ src/helpdesk/views/auth.py
@@ -4,23 +4,52 @@ from django.contrib.auth.views import (
     LogoutView,
     PasswordChangeDoneView,
     PasswordChangeView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
     redirect_to_login,
 )
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import resolve_url
 from django.urls import reverse_lazy
 
-default_login_view = LoginView.as_view(template_name="helpdesk/registration/login.html")
 
-logout = LogoutView.as_view(template_name="helpdesk/registration/logged_out.html")
+def _t(fragment: str) -> str:
+    return f"helpdesk/registration/{fragment}"
+
+
+default_login_view = LoginView.as_view(template_name=_t("login.html"))
+
+logout = LogoutView.as_view(template_name=_t("logged_out.html"))
 
 password_change = PasswordChangeView.as_view(
-    template_name="helpdesk/registration/change_password.html",
+    template_name=_t("change_password.html"),
     success_url=reverse_lazy("helpdesk:password_change_done"),
 )
 
 password_change_done = PasswordChangeDoneView.as_view(
-    template_name="helpdesk/registration/change_password_done.html"
+    template_name=_t("change_password_done.html")
+)
+
+password_reset = PasswordResetView.as_view(
+    template_name=_t("password_reset_form.html"),
+    email_template_name=_t("password_reset_email.html"),
+    subject_template_name=_t("password_reset_subject.txt"),
+    success_url=reverse_lazy("helpdesk:password_reset_done"),
+)
+
+password_reset_done = PasswordResetDoneView.as_view(
+    template_name=_t("password_reset_done.html")
+)
+
+password_reset_confirm = PasswordResetConfirmView.as_view(
+    template_name=_t("password_reset_confirm.html"),
+    success_url=reverse_lazy("helpdesk:password_reset_complete"),
+)
+
+password_reset_complete = PasswordResetCompleteView.as_view(
+    template_name=_t("password_reset_complete.html")
 )
 
 
